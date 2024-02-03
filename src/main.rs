496b1634f8570cb9551af295bcbfdb2d701c7f30
PaulBenHill <benhill70@yahoo.com>
2024-02-03 18:01:21 -0500

    Inital write report to disk started

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use std::env;
 use std::fs::File;
 use std::io::Lines;
 use std::io::{BufRead, BufReader};
 use std::path::*;
+use std::{env, fs};
 
 mod parsers;
 use crate::parser_model::FileDataPoint;
@@ -13,25 +13,104 @@ mod parser_model;
 mod reports;
 use crate::reports::*;
 
+const OUTPUT_DIR: &str = "output";
+
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let working_dir = env::current_dir().unwrap().clone();
+    println!("The current directory is {}", working_dir.display());
+
+    if !create_output_dir() {
+        std::process::exit(-1);
+    }
+
+    let args: Vec<String> = env::args().skip(1).collect();
 
     for file in args {
-        let path = verify_file(&file);
+        let result = verify_file(&file);
+        let file_name = result.0.file_name().unwrap().to_str().unwrap();
 
-        let reader = open_log_file(path);
+        let reader = open_log_file(result.0);
 
         let lines = reader.lines();
 
         let reports = process_lines(lines);
+
+        let report_dir = create_report_dir(&working_dir, file_name, result.1);
+
+        write_reports(&report_dir, reports.0, reports.1);
     }
 }
 
-fn verify_file(filename: &String) -> &Path {
+fn create_report_dir(working_dir: &PathBuf, filename: &str, file_size: u64) -> PathBuf {
+    let mut report_dir_name = String::new();
+    report_dir_name.push_str(filename);
+    report_dir_name.push_str(&file_size.to_string());
+    report_dir_name = report_dir_name
+        .chars()
+        .filter(|c| !c.is_whitespace())
+        .collect();
+
+    let report_dir: PathBuf = [
+        working_dir,
+        Path::new(OUTPUT_DIR),
+        Path::new(&report_dir_name),
+    ]
+    .iter()
+    .collect();
+
+    if !report_dir.exists() {
+        match fs::create_dir(&report_dir) {
+            Ok(_) => (),
+            Err(e) => panic!("Unable to create report dir: {:?}:{:?}", report_dir, e),
+        }
+    }
+
+    println!("Report directory: {:?}", report_dir);
+    report_dir.clone()
+}
+
+fn write_reports(
+    report_dir: &PathBuf,
+    parsed_lines: Vec<FileDataPoint>,
+    damage_report: DamageReport,
+) -> bool {
+    true
+}
+
+fn create_output_dir() -> bool {
+    let result: bool = match fs::canonicalize(OUTPUT_DIR) {
+        Ok(path) => {
+            println!("Output dir: {:?}", path);
+            true
+        }
+        Err(e) => match fs::create_dir(OUTPUT_DIR) {
+            Ok(output_dir) => {
+                println!(
+                    "Output directory did not exist. Creating dir: {:?}",
+                    output_dir
+                );
+                true
+            }
+            Err(e) => {
+                eprintln!("Unable to create output directory: {:?}", e);
+                false
+            }
+        },
+    };
+    result
+}
+
+fn verify_file(filename: &String) -> (&Path, u64) {
     let path = Path::new(filename);
 
     if path.exists() {
-        path
+        match fs::metadata(path) {
+            Ok(meta) => (path, meta.len()),
+            Err(e) => panic!(
+                "Cannot retrieve metadata. Probably permissions issue: {:?}",
+                e
+            ),
+        }
     } else {
         panic!("Cannot find file");
     }
@@ -42,7 +121,7 @@ fn open_log_file(path: &Path) -> BufReader<File> {
         Ok(file) => file,
         Err(e) => panic!("Unable to open file {:?}", e),
     };
-    println!("{:?}", file);
+    println!("File opened for processing: {:?}", file);
     BufReader::new(file)
 }
 
@@ -55,7 +134,7 @@ fn process_lines(lines: Lines<BufReader<File>>) -> (Vec<FileDataPoint>, DamageRe
         line_count += 1;
         for p in &parsers {
             if let Some(data) = p(line_count, &line) {
-                println!("{:?}", data);
+                //println!("{:?}", data);
                 data_points.push(data);
                 break;
             }
